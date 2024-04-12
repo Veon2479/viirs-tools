d37ba370faf008eeeb1091e66e7c650e4bcbf386
Whiskeyjack <andriei.shuliak@gmail.com>
2024-04-13 01:51:37 +0300

    Updated project info

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('LICENSE', 'r') as f:
+    license_text = f.read()
+
 setup(
     name='viirs-tools',
     description='Python library for processing VIIRS data',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/Veon2479/viirs-tools',
+    license=license_text,
     author='Andrey Shuliak',
     version='0.1.0',
     packages=find_packages(),
@@ -11,5 +21,13 @@ setup(
     ],
     extras_require={
         'assimilator': ['netcdf4']
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Atmospheric Science'
+    ],
+    keywords=['viirs', 'satellite', 'remote sensing', 'meteorology'],
+
 )
